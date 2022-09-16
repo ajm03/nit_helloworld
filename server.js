e1f0f8998bfdc790bf02e63fe7cfeccc0b10463f
ajmal <46993516+ajm03@users.noreply.github.com>
2022-09-16 15:51:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
  response.writeHead(200);
- response.end("Hey How are you Today..!14");
+ response.end("Hey How are you Today NIT####..!15");
 
 }
 var www = http.createServer(handleRequest);
